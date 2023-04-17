7cc38faae27c55d1c91f3fc4a8dd28bc5a3d1a20
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2023-04-17 23:16:43 +0200

    Still untested. But at least now it does something.

--- tdx_commit.c
+++ tdx_commit.c
@@ -16,8 +16,10 @@ extern "C" {
 int tdx_commit
   (tdx_t* tdx)
 {
+  int r;
   tdc_t cursor;
   tdt_t key, val;
+  unsigned char* valuedata;
 
   CHECK(tdc_init(&(tdx->changes), &cursor));
   while (1) {
@@ -26,6 +28,29 @@ int tdx_commit
     val.data = 0;
     val.size = 0;
     if (tdc_nxt(&cursor, &key, &val, TDFLG_ALLOCTDT) != 0) {
+      break;
+    }
+    if (value->size == 0) {
+      free(key.data);
+      free(val.data);
+      return TDERR_STRUCT;
+    }
+    valuedata = val.data;
+    switch (valuedata[ value->size - 1 ]) {
+    case TDX_PUT:
+      val.size -= 1;
+      if ((r = td_put(tdx->orig, &key, &val, 0)) != 0) {
+        free(key.data);
+        free(val.data);
+        return r;
+      }
+      break;
+    case TDX_DEL:
+      if ((r = td_del(tdx->orig, &key, 0, 0)) != 0) {
+        free(key.data);
+        free(val.data);
+        return r;
+      }
       break;
     }
     free(key.data);
