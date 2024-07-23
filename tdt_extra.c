7b98410a335754d593d4cc731503d8cbc2432776
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2024-07-23 10:45:08 +0200

    Correctness fix.

--- tdt_extra.c
+++ tdt_extra.c
@@ -37,7 +37,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <string.h>
 #include <stdint.h>
 
-#include "td.h"
+#include "td_private.h"
 
 void tdt_printf
   (tdt_t* out, char* fmt, ...)
